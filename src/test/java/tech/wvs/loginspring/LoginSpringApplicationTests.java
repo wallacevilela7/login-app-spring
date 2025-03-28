fe2544cb6e4a77c8d35198aabbb0b7718680b112
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-28 14:13:24 -0300

    Create spring app

---
+++ src/test/java/tech/wvs/loginspring/LoginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.loginspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
